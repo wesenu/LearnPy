5f18735b9d2c8ec26fd12e4cbfed862cc8ffd0d6
Abubakar Ahmed <abubakarahmed4497@gmail.com>
2021-12-27 17:59:50 +0530

    Chaining to Linux

--- Python How to Program/Chapter 5/Encryption_Key.py
+++ Python How to Program/Chapter 5/Encryption_Key.py
@@ -1,4 +1,3 @@
-
 # Write a function that accepts a string as input
 # isolates the different unique letters
 
@@ -6,7 +5,14 @@
 # ignore ignoring punctuation, spaces, and case sensitivity
 # use lower() for case sensitive string
 
+import string
+
+def GetKey(myInput):
+    return myInput.translate(None, string.punctuation)
 
 
 
 user_input = input('Enter your string: ')
+
+new_user_input = GetKey(user_input)
+print(new_user_input)
